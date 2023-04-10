1385fdbc4578aa0a9eee1aca291bbb4f891415e6
Mark Donaldson 黑龍/黑龙 <va3mhd@gmail.com>
2023-04-09 22:53:13 -0400

    Signing off for April 9, 2023. Will continue on April 10, 2023 foro Basics Codelab for Android programming.

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,29 +20,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-private fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(text = "Hello $name!")
-    }
-}
-
-@Preview(showBackground = true, name = "Text preview")
-@Composable
-fun DefaultPreview() {
-    BasicsCodelabTheme {
+private fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
         Greeting("Android")
     }
 }
+
+@Composable
+private fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        Surface(color = MaterialTheme.colorScheme.primary) {
+            Column(modifier = Modifier.padding(38.dp)){
+            Column {
+                Text("Hello,")
+                Text("Android")
+            }
+            }
+        }
+    }
+}
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun DefaultPreview() {
+        BasicsCodelabTheme {
+            MyApp()
+        }
+    }

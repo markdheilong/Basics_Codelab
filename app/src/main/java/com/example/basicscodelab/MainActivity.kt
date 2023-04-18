0d822516995e5e7dedb2f413e5a202f5bcb78fe0
Mark Donaldson 黑龍/黑龙 <va3mhd@gmail.com>
2023-04-17 23:09:37 -0400

    Signing off for April 17, 2023. Will continue again this week working on more Google Codelabs for Jetpack Compose.

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,35 +26,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting("Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 private fun Greeting(name: String) {
     Surface(
-        color = MaterialTheme.colorScheme.primary
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Surface(color = MaterialTheme.colorScheme.primary) {
-            Column(modifier = Modifier.padding(38.dp)){
-            Column {
-                Text("Hello,")
-                Text("Android")
-            }
+        Row(modifier = Modifier.padding(24.dp)){
+            Column(modifier = Modifier.padding(24.dp)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+        }
+            ElevatedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
             }
         }
     }
 }
 
-    @Preview(showBackground = true)
-    @Composable
-    private fun DefaultPreview() {
-        BasicsCodelabTheme {
-            MyApp()
-        }
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun DefaultPreview() {
+    BasicsCodelabTheme {
+        MyApp()
     }
+}
